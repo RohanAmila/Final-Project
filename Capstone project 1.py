6a175850d75761dd98fd667f1996be5d4c34c822
RohanAmila <52007981+RohanAmila@users.noreply.github.com>
2019-06-29 15:03:24 +0400

    Add files via upload

---
+++ Capstone project 1.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# # This Note Book Contains My Capstone project data
+# 
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hellow Capstone Project Course")
+
